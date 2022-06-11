3faa4848f6fa399a00a9561bce78cb1e3bad8079
Marco Cossai <marcocossai4697@gmail.com>
2022-06-11 13:55:38 +0200

    refs #09 - fix varie

--- helper/functions.php
+++ helper/functions.php
@@ -1931,25 +1931,22 @@ function newAccessories() {
     //Prelevo i dati dal form
     $idPlant_FK = $_POST['IdImpianto_FK'];
     $idCustomer_FK = $_POST['IdCustomer_FK'];
-    $POSModel = NVL(addslashes($_POST['ModelloPOS']), NULL);
-    $TID = NVL($_POST['TID'],NULL);
-    $IFSFVersion = NVL(addslashes($_POST['VersioneIFSF']),NULL);
-    $ipPOS = NVL(addslashes($_POST['IP_POS']),NULL);
-    $qntRFID = NVL($_POST['QNT_RFID'],NULL);
-    $ipGTW = NVL($_POST['IP_GTW'],NULL);
-    $MediaSmart = NVL($_POST['MediaSmart'],NULL);
-    $printer = NVL($_POST['Stampanti'],NULL);
-    $ipSafetySmart = NVL(addslashes($_POST['IpSafetySmart']),NULL);
-    $backup = NVL(addslashes($_POST['Backup']),NULL);
+    $POSModel = $_POST['ModelloPOS'];
+    $TID = $_POST['TID'];
+    $IFSFVersion = $_POST['VersioneIFSF'];
+    $ipPOS = $_POST['IP_POS'];
+    $qntRFID = $_POST['QNT_RFID'];
+    $ipGTW = $_POST['IP_GTW'];
+    $MediaSmart = $_POST['MediaSmart'];
+    $printer = $_POST['Stampanti'];
+    $ipSafetySmart = $_POST['IpSafetySmart'];
+    $backup = $_POST['Backup'];
     $dataLog = date("d/m/Y - H:i:s");
 
     //Prelevo i dati dell'utente
     $usernameSession = $_SESSION['Username'];
     $result = mysqli_query(connDB(),"SELECT `Nome`,`Cognome` FROM `utenti` WHERE BINARY `Username` = '$usernameSession'") or die (mysqli_error(connDB()));
-    if($row = mysqli_fetch_array($result)) {
-        $currentUser = $row['Nome'] . " " . addslashes($row['Cognome']);
-    }
-
+	
     //Prelevo il nome dell'impianto
     $result = mysqli_query(connDB(),"SELECT `NomeImpianto` FROM `impianti` WHERE `IdImpianto` = $idPlant_FK") or die (mysqli_error(connDB()));
     if($row = mysqli_fetch_array($result)) {
@@ -1960,7 +1957,7 @@ function newAccessories() {
     $message = addslashes("Sono stati aggiunti accessori all'impianto $namePlant");
     
     //Inserisco gli accessoro
-    $result = mysqli_query(connDB(),"INSERT INTO `accessori` VALUES (0,'$POSModel',$TID,CAST('$IFSFVersion' AS DECIMAL(10,0)),'$ipPOS',$MediaSmart,$printer,'$ipSafetySmart',$qntRFID,'$ipGTW','$backup',$idPlant_FK)") or die (mysqli_error(connDB()));
+    $result = mysqli_query(connDB(),"INSERT INTO `accessori` VALUES (0,'$POSModel','$TID','$IFSFVersion','$ipPOS','$MediaSmart','$printer','$ipSafetySmart','$qntRFID','$ipGTW','$backup',$idPlant_FK)") or die (mysqli_error(connDB()));
     if($result) {
         //Inserisco il log
         mysqli_query(connDB(),"INSERT INTO `log` VALUES (0,'$dataLog','$message',1,'$currentUser')") or die (mysqli_error(connDB()));
